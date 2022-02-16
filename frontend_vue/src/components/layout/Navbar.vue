02c0793565d5068c81bfa0d3f2b080b94f726f25
KamranMoazim <kamrannaseer765@gmail.com>
2022-02-16 12:41:06 +0500

    section-1 completed

---
+++ frontend_vue/src/components/layout/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>
+                    DRF VUE CRM
+                </strong>
+            </router-link>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/sign-up"  class="button is-success"><strong>SignUp</strong></router-link>
+                        <router-link to="/log-in"  class="button is-light">Log In</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+    export default {
+        name:"Navbar"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ frontend_vue/src/views/LogIn.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="column">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">
+                    Log In
+                </h1>
+                <form>
+                    <div class="field">
+                        <label>Email</label>
+                        <div class="control">
+                            <input class="input" type="email" name="email" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label>Password</label>
+                        <div class="control">
+                            <input class="input" type="password" name="password1" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-success">Login</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:"LogIn"
+    }
+</script>
